a854f767234998a3a49a73688f87cfa9c76aef16
puliv <pverapavez@gmail.com>
2024-05-04 22:33:18 -0400

    Agregando primera actividad 'Hotel Paradise'

---
+++ HotelParadise.py
@@ -0,0 +1,108 @@
+# Hotel "Paradise Dreams"
+import os, time
+
+numeroReserva = 0
+nombreHuesped = ""
+direccion = ""
+correo = ""
+edad = 0
+numeroAcompanantes = 0
+admin = None
+passAdm = None
+
+while True:
+    os.system("cls")
+    print("*****Sistema de Gestión de Huéspedes******")
+    print("1.- Registrar Huésped")
+    print("2.- Consultar Datos Huésped")
+    print("3.- Crear Usuario Admin")
+    print("4.- Salir")
+    try:
+        opcion = int(input("Ingresa opción\n"))
+        if opcion == 1:
+            print("*****Registrar Huésped*****")
+            time.sleep(1)
+            numeroReserva = int(input("Ingresa Número de Reserva\n"))
+            while numeroReserva < 1000 or numeroReserva > 9999:
+                numeroReserva = int(input("Ingresa un número de reserva válido\n"))
+
+            nombreHuesped = input("Ingresa Nombre del huésped\n")
+            while nombreHuesped == "":
+                nombreHuesped = input("Ingresa nombre del huésped\n")
+
+            direccion = input("Ingresa dirección\n")
+            while direccion == "":
+                direccion = input("Ingresa dirección válida\n")
+
+            correo = input("Correo electrónico\n")
+            while "@" not in correo:
+                correo = input("Correo electrónico\n")
+
+            edad = int(input("Edad\n"))
+            if edad < 18 and edad > 120:
+                edad = int(input("Ingresa edad entre 18 y 120 años\n"))
+
+            numeroAcompanantes = int(input("Número de acompañantes\n"))
+            if numeroAcompanantes <= 0:
+                numeroAcompanantes = int(
+                    input("Número de acompañantes debe ser minimo 1\n")
+                )
+
+            print("******Reserva creada con exito******")
+            next = input("Presione enter para continuar\n\n")
+        elif opcion == 2:
+            print("*****Consulta Datos Huesped*****")
+            time.sleep(1)
+
+            adminUser = input("Ingrese nombre de usuario\n")
+            while adminUser == "" or adminUser != admin:
+                adminUser = input("Ingrese un nombre de usuario valido\n")
+
+            adminPass = input("Ingrese contraseña de usuario\n")
+            while adminPass == "" or adminPass != passAdm:
+                adminPass = int(input("Ingrese contraseña de usuario válida\n"))
+
+            print("*****Admin ingresado con exito*****")
+            reservaIngresada = int(input("Ingresa numero de reserva\n"))
+            while reservaIngresada < 1000 or reservaIngresada > 9999:
+                reservaIngresada = int(input("Ingresa un numero de reserva válido\n"))
+            if reservaIngresada == numeroReserva:
+                print(
+                    f"Nombre Huesped: {nombreHuesped},\nDireccion: {direccion},\nCorreo: {correo},\nEdad: {edad}"
+                )
+                if numeroAcompanantes > 3:
+                    print(
+                        f"Numero Acompañantes: {numeroAcompanantes} (Huesped con demasiados acompañantes)"
+                    )
+                else:
+                    print(
+                        f"Numero Acompañantes:{numeroAcompanantes} (Huesped dentro del limite de acompañantes)"
+                    )
+            else:
+                print("Número de reserva no existe")
+
+            next = input("presione enter para continuar\n")
+        elif opcion == 3:
+            print("*****Crear Admin*****")
+            time.sleep(1)
+
+            admin = input("Ingrese nuevo nombre de usuario\n")
+            while admin == "":
+                admin = input("Ingrese nuevo nombre de usuario valido\n")
+
+            passAdm = input("Ingrese nueva contreaseña\n")
+            while passAdm == "":
+                passAdm = input("Ingrese nueva contreaseña valida\n")
+
+            if admin != "" and passAdm != "":
+                print("******Usuario admin creado con exito******")
+
+            next = input("Presione enter para continuar\n")
+        elif opcion == 4:
+            print("*****Salir*****")
+            print("Gracias por preferir Hotel 'Paradise Dreams'")
+            break
+        else:
+            print("Opcion no valida")
+    except:
+        print("opcion no valida")
--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # aprendiendoPython
 Repositorio para guardar los ejercicios desarrollados con Python en clases de programación
+
+Hotel Paradise:
+El Hotel "Paradise Dreams" desea implementar un sistema informático para la gestión
+de sus huéspedes. A continuación, se detallan los requisitos para el desarrollo:
+Se te solicita crear un menú en el cual se puedan registrar los datos de un huésped. Los
+atributos que debe tener la ficha son los siguientes:
+Número de Reserva
+Nombre del huésped
+Dirección
+Correo electrónico
+Edad
+Número de acompañantes
+
+El menú debe tener las siguientes opciones:
+
+Sistema de Gestión de Huéspedes
+1) Registrar Huésped
+2) Consultar Datos de Huésped
+3) Salir
+Donde:
+Registrar Huésped: Solicita todos los datos de un huésped para hacer el registro de una
+nueva ficha. Cada uno de los atributos debe cumplir con lo solicitado mediante
+validación.
+- Nombre del huésped: no puede estar vacío.
+- Dirección: no puede estar vacía.
+- Número de Reserva: debe ser un número entero que se encuentre dentro del rango
+de 1000 y 9999.
+- Edad: debe ser un número entero que se encuentre en el rango de 18 a 120.
+- Correo electrónico: debe ser una cadena de caracteres que contenga al menos un
+carácter "@".
+- Número de acompañantes: debe ser un número entero mayor o igual a cero.
+Consultar Datos de Huésped:
+Muestra por pantalla todos los atributos del huésped que coincidan con el número de
+reserva ingresado. Los datos se deben mostrar de forma ordenada, utilizando
+herramientas de tabulación y/o saltos de línea según lo aprendido en clases (ver reglas
+de negocio más abajo).
+Salir:
+Sale del ciclo del menú y muestra un mensaje "Gracias por preferir Hotel 'Paradise
+Dreams' ".
+Reglas de negocio:
+Debe crearse un usuario y una contraseña para el administrador. Solo este usuario
+tiene permisos para visualizar la opción 2.
+- Ejemplo:
+- Usuario: seller
+- Contraseña: 123
+En el resumen (opción 2), deberá desplegarse toda la información del huésped y, si el
+número de acompañantes es superior a 3, el sistema deberá indicar 'huésped con
+demasiados acompañantes’; caso contrario, 'huésped dentro de límite de
+acompañantes'.
