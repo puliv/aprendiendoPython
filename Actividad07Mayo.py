ecbce21f9bd45d9911fe5e9d9a52cbaa7859dee1
puliv <pverapavez@gmail.com>
2024-06-07 23:34:29 -0400

    descomentando funciones y agregando condiciones

--- Actividad07Mayo.py
+++ Actividad07Mayo.py
@@ -1,70 +1,112 @@
+import math, time, os
+
+os.system("cls")
+
 # función que calcule el total de una factura. La función debe recibir la cantidad sin IVA,
 # y devolver el total de la factura (recordemos que en Chile el IVA tiene el valor del 19%).
-import math
 
-# def precioIVA(valor):
-#     # print("valor bruto", valor)
-#     iva = 19 / 100
-#     valorImpuesto = valor * iva
-#     # print("valor neto", valorImpuesto)
-#     valorNeto = valor + valorImpuesto
-#     return int(valorNeto)
-# valorBruto = int(input("Ingrese valor bruto (Sin impuestos)\n"))
-# print(precioIVA(valorBruto))
+print("******funcion impuestos******")
 
+
+def precioIVA(valor):
+    iva = 19 / 100
+    valorImpuesto = valor * iva
+    valorNeto = valor + valorImpuesto
+    return print("Valor ingresado + impuestos (19%): ", int(valorNeto))
+
+
+valorBruto = int(input("Ingrese valor bruto (Sin impuestos)\n"))
+precioIVA(valorBruto)
+
+
+time.sleep(1)
 # función que calcule el área de un círculo
-# def areaCirculo():
-#     radio = float(input("Escribe el radio: \n"))
-#     # Calculando el área
-#     area = math.pi * radio**2
-#     print(f"El área del círculo es: {area:.2f}")
-# areaCirculo()
-
-# función que reciba una muestra de números en una lista y devuelva su media.
+print("******funcion area circulo******")
 
 
-# listaNum = []
-# listCuadrada = []
-# while True:
-
-#     def mediaNums(num):
-#         listaNum.append(num)
-#         media = sum(listaNum) / len(listaNum)
-#         if len(listaNum) > 2:
-#             print(listaNum)
-#             return "la media es ", int(media)
-#         else:
-#             return "Sigue ingresando numeros"
-
-#     valor = int(input("Ingresa numero\n"))
-#     print(mediaNums(valor))
+def areaCirculo():
+    radio = float(input("Ingresa el radio de un circulo: \n"))
+    # Calculando el área
+    area = math.pi * radio**2
+    return print(f"El área del círculo es: {area:.2f}")
 
 
-# función que reciba una muestra de números en una lista y devuelva otra lista con sus cuadrados.
-# def handleArrays(num):
-#     listaNum.append(num)
-#     cuadrado = num**2
-#     listCuadrada.append(cuadrado)
-#     print("listCuadrada", listCuadrada)
-#     print("listaNum", listaNum)
+areaCirculo()
 
-
-# numero = int(input("Ingrense numero \n"))
-# handleArrays(numero)
-
+time.sleep(1)
 # función que reciba una cadena de caracteres y devuelva la cantidad de palabras
+print("******funcion que devuelve la cantidad de palabras******")
 
 
 def cantidadPalabras(txt):
     listaPalabras = txt.split()
-    return len(listaPalabras)
-
-print("el texto contiene ", cantidadPalabras(input("escribe\n")), " palabras")
+    print("el texto contiene ", len(listaPalabras), " palabras")
 
 
+cantidadPalabras(input("ingresa un texto:\n"))
+
+
+time.sleep(1)
 # función que reciba una cadena de caracteres y devuelva la cantidad de letras que contiene la frase
-# def cantidadLetras(palabra):
-#     print(len(palabra))
+print("******funcion que devuelve la cantidad de letras******")
 
 
-# cantidadLetras(input("escribe\n"))
+def cantidadLetras(palabra):
+    print("El texto tiene ", len(palabra), " letras")
+
+
+cantidadLetras(input("ingresa un texto:\n"))
+
+listaNum = []
+numList = []
+listCuadrada = []
+funcionMediaAbierto = True
+funcionCuadradoAbierto = True
+time.sleep(1)
+print("******funcion que devuelve la media de la lista******")
+while funcionMediaAbierto:
+    # función que reciba una muestra de números en una lista y devuelva su media.
+
+    def mediaNums(num):
+        listaNum.append(num)
+        media = sum(listaNum) / len(listaNum)
+        if len(listaNum) > 2:
+            print(listaNum)
+            return "la media es ", int(media)
+        else:
+            return "Sigue ingresando numeros"
+
+    if len(listaNum) > 2:
+        seguir = int(input("Quieres continuar? 1) Si 2) No:\n"))
+        if seguir == 1:
+            valor = int(input("Ingresa numero\n"))
+        else:
+            funcionMediaAbierto = False
+    else:
+        valor = int(input("Ingresa numero\n"))
+    print(mediaNums(valor))
+
+time.sleep(1)
+# función que reciba una muestra de números en una lista y devuelva otra lista con sus cuadrados.
+print("******funcion que devuelve lista con numeros ingresados al cuadrado******")
+while funcionCuadradoAbierto:
+
+    def handleArrays(num):
+        numList.append(num)
+        cuadrado = num**2
+        listCuadrada.append(cuadrado)
+        print("listCuadrada", listCuadrada)
+        print("numList", numList)
+
+    if len(numList) > 2:
+        seguir = int(input("Quieres continuar? 1) Si 2) No:\n"))
+        if seguir == 1:
+            numero = int(input("Ingresa numero\n"))
+        else:
+            funcionCuadradoAbierto = False
+    else:
+        numero = int(input("Ingresa numero\n"))
+    handleArrays(numero)
+
+
+print("************FIN************")
